d2d0d5e02ca616d69f4b4789a0a644436c4f3f15
Carlos A. Crespo <lvccrespo@gmail.com>
2022-04-24 16:33:16 -0300

    add tests for logged_exec()

--- tests/test_common.py
+++ tests/test_common.py
@@ -4,9 +4,10 @@
 
 """Common functions module tests."""
 
+import logging
 import pytest
 
-from pyempaq.common import find_venv_bin
+from pyempaq.common import logged_exec, find_venv_bin
 
 
 def test_find_venv_bin_l(tmp_path):
@@ -29,3 +30,27 @@ def test_find_venv_bin_no(tmp_path):
     """Can't find directory for the executable and raise RuntimeError."""
     with pytest.raises(RuntimeError):
         find_venv_bin(tmp_path, "pip_baz")
+
+
+def test_logged_exec(caplog):
+    """Execute a command, redirecting the output to the log. Everything OK."""
+    with caplog.at_level(logging.DEBUG):
+        stdout = logged_exec(['echo', 'test'])
+
+    assert stdout == ['test']
+    assert "Executing external command: ['echo', 'test']" in caplog.text
+
+
+def test_logged_exec_error():
+    """Execute a command, raises an error."""
+    with pytest.raises(Exception) as e:
+        logged_exec(["pip_bar", "pip_baz"])
+
+    assert "Command ['pip_bar', 'pip_baz'] crashed with " in str(e.value)
+
+
+def test_logged_exec_wait():
+    """Execute a command and ended with some return code."""
+    with pytest.raises(Exception) as e:
+        logged_exec(['dir', '$?'])
+    assert str(e.value) == "Command ['dir', '$?'] ended with retcode 2"
