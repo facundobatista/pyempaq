fc2af50c666e8c2089b59fda38f129a493455480
Facundo Batista <facundo@taniquetil.com.ar>
2023-03-25 14:39:40 -0300

    Add 'include' and 'exclude' configuration options to have control on what is inside the packed file.

--- pyempaq/main.py
+++ pyempaq/main.py
@@ -5,15 +5,19 @@
 """Main packer module."""
 
 import argparse
+import errno
+import glob
 import json
 import logging
-import pathlib
+import os
 import shutil
 import tempfile
 import uuid
 import venv
 import zipapp
 from collections import namedtuple
+from pathlib import Path
+from typing import List
 
 from pyempaq import __version__
 from pyempaq.common import find_venv_bin, logged_exec, ExecutionError
@@ -31,7 +35,7 @@ Args = namedtuple("Args", "project_name basedir entrypoint requirement_files")
 
 def get_pip():
     """Ensure an usable version of `pip`."""
-    useful_pip = pathlib.Path("pip3")
+    useful_pip = Path("pip3")
     # try to see if it's already installed
     try:
         logged_exec([useful_pip, "--version"])
@@ -42,21 +46,97 @@ def get_pip():
         return useful_pip
 
     # no useful pip found, let's create a virtualenv and use the one inside
-    tmpdir = pathlib.Path(tempfile.mkdtemp())
+    tmpdir = Path(tempfile.mkdtemp())
     venv.create(tmpdir, with_pip=True)
     useful_pip = find_venv_bin(tmpdir, "pip3")
     return useful_pip
 
 
+def copy_project(src_dir: Path, dest_dir: Path, include: List[str], exclude: List[str]):
+    """Copy/link the selected project content from the source to the destination directory.
+
+    The content is selected with the 'include' list of patterns, minus the 'exclude' ones.
+
+    It works differently for the different node types:
+    - regular files: hard links (unless permission error or cross device)
+    - directories: created
+    - symlinks: respected, validating that they don't link to outside
+    - other types (blocks, mount points, etc): ignored
+    """
+    included_nodes = ["."]  # always the root, to create the destination directory
+    for pattern in include:
+        included_nodes.extend(glob.iglob(pattern, root_dir=src_dir, recursive=True))
+
+    # need to remove all content inside symlinked directories (as that symlink will
+    # be reproduced, so those contents don't need to be particularly handled)
+    symlinked_dirs = set()
+    for node in included_nodes:
+        node = src_dir / node
+        if node.is_dir() and node.is_symlink():
+            symlinked_dirs.add(node)
+    included_nodes = [
+        node for node in included_nodes
+        if not any(parent in symlinked_dirs for parent in (src_dir / node).parents)
+    ]
+
+    # get all excluded nodes, building the source path so it's comparable below
+    excluded_nodes = set()
+    for pattern in exclude:
+        excluded_nodes.update(
+            src_dir / path
+            for path in glob.iglob(pattern, root_dir=src_dir, recursive=True)
+        )
+
+    def _relative(node):
+        """Return str'ed node relative to src_dir, ready to log."""
+        return str(node.relative_to(src_dir))
+
+    for node in included_nodes:
+        src_node = src_dir / node
+        dest_node = dest_dir / node
+
+        if src_node in excluded_nodes:
+            logger.debug("Ignoring excluded node: %r", _relative(src_node))
+            continue
+        if any(parent in excluded_nodes for parent in src_node.parents):
+            logger.debug("Ignoring node because excluded parent: %r", _relative(src_node))
+            continue
+
+        if src_node.is_symlink():
+            real_pointed_node = src_node.resolve()
+            if src_dir not in real_pointed_node.parents:
+                logger.debug(
+                    "Ignoring symlink because targets outside the project: %r -> %r",
+                    _relative(src_node), str(real_pointed_node),
+                )
+                continue
+            relative_link = os.path.relpath(real_pointed_node, src_node.parent)
+            dest_node.symlink_to(relative_link)
+
+        elif src_node.is_dir():
+            dest_node.mkdir(mode=src_node.stat().st_mode, exist_ok=True)
+
+        elif src_node.is_file():
+            try:
+                dest_node.hardlink_to(src_node)
+            except OSError as error:
+                if error.errno != errno.EXDEV and not isinstance(error, PermissionError):
+                    raise
+                shutil.copy2(src_node, dest_node)
+
+        else:
+            logger.debug("Ignoring file because of type: %r", _relative(src_node))
+
+
 def pack(config):
     """Pack."""
-    project_root = pathlib.Path(__file__).parent
-    tmpdir = pathlib.Path(tempfile.mkdtemp())
+    project_root = Path(__file__).parent
+    tmpdir = Path(tempfile.mkdtemp())
     logger.debug("Working in temp dir %r", str(tmpdir))
 
     # copy all the project content inside "orig" in temp dir
     origdir = tmpdir / "orig"
-    shutil.copytree(config.basedir, origdir)
+    copy_project(config.basedir, origdir, config.include, config.exclude)
 
     # copy the common module
     pyempaq_dir = tmpdir / "pyempaq"
@@ -120,7 +200,7 @@ def main():
     """Manage CLI interaction and call pack."""
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "source", type=pathlib.Path,
+        "source", type=Path,
         help="The source file (pyempaq.yaml) or the directory where to find it.")
     parser.add_argument(
         '-v', '--verbose',
